c069ebbbf9cf5f1bcd815c36b59d18713aa81013
황윤희 <a50632571@gmail.com>
2022-01-17 07:38:59 +0000

    인덱스 추가

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 from flask import Flask, render_template, request
+import os
+from werkzeug.utils import secure_filename
+
+UPLOAD_DIR = "/Users/gidaehyeon/Downloads/PreviewContentTransition-main/venv/static"
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
@@ -8,10 +13,18 @@ def index():
 @app.route('/index1', methods=['GET', 'POST'])
 def index1():
     if request.method == 'POST':
+        return render_template('index1.html')
+    
+@app.route('/index2', methods=['GET', 'POST'])
+def index2():
+    if request.method == 'POST':
         f = request.files['file']
-        names = request.form['naming']
         filnames = str(f.filename)
-        return render_template('index1.html', data1=f)
+        filenames = str(f.filename.split('.')[-2])
+        #path = os.path.join(app.config['UPLOAD_DIR'], filnames)
+        f.save("/Users/gidaehyeon/"+secure_filename(f.filename))
+        #f.save(secure_filename('/Users/gidaehyeon/Downloads/PreviewContentTransition-main/venv/static/img/' + secure_filename(filnames)))
+        return render_template('index2.html',data1=f, data2=filnames, data3=filenames)
 
 if __name__ ==  '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=8000)
